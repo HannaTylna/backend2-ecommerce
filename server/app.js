538d7d0ebb52edf7d2fbbbc9734f73f358b5d71e
Hanna <hanna.tylna@gmail.com>
2022-04-01 19:49:33 +0200

    add product routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const { productRoutes } = require("./controllers/products.js");
+
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.get("/", (req, res) => {
   res.send("Success!");
 });
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
